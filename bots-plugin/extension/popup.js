e7bed6952859ee7ad3c962d3f9388ddc81b22dc1
Jordy de Koning <jordy@nefkon.com>
2016-11-10 14:49:53 +0100

    template browser plugin finished, javascript time

--- bots-plugin/extension/popup.js
+++ bots-plugin/extension/popup.js
@@ -15,9 +15,8 @@ document.addEventListener('DOMContentLoaded', function() {
       // f.appendChild(i);
       // maak grafiek met f
       // d.body.appendChild(d.body.innerHTML = 'test');
-      $.post( "http://peterschriever.com/api/v1/legal-text-check", function( result ) {
+      $.post( "http://peterschriever.com/api/v1/csharp-legal-text-check", function( result ) {
       // will generate array with ['Monday', 'Tuesday', 'Wednesday']
-      // alert(JSON.stringify(result));
       var labels = $.map(result.data, function(category) {
         return category.name;
       });
@@ -35,24 +34,49 @@ document.addEventListener('DOMContentLoaded', function() {
       });
       // alert(JSON.stringify(status));
 
+      var description = $.map(result.data, function(category) {
+        // return category.paragraphs;
+        // var par = '';
+        var par = {};
+        par[category.name] = $.map(category.problems, function(problem) {
+          return problem.msg;
+        //
+        });
+        return par;
+      });
+
       var data = {
         datasets: [
         {
           label: labels,
-          fillColor: "rgba(220,220,220,0.5)",
-          strokeColor: "rgba(220,220,220,0.8)",
-          highlightFill: "rgba(220,220,220,0.75)",
-          highlightStroke: "rgba(220,220,220,1)",
           data: status
         }
         ]
       };
 
+      // alert(JSON.stringify(description));
+
+      $.each(description, function(index, cats) {
+        $.each(cats, function(index, value) {
+
+          // alert(index + ":" + JSON.stringify(value));
+        });
+      });
+
+
+
+
+      // $("#description").append(JSON.stringify(description));
+
       // toggle display elements
-      $('.text-center').toggle();
+      $('.button-tos').toggle();
 
       var ctx = document.getElementById("myChart");
-      $('#myChart').toggle();
+
+      $('#result').toggle();
+
+        $("#app").addClass("app-result");
+
       var myChart = new Chart(ctx, {
         type: 'pie',
         data: {
@@ -62,18 +86,12 @@ document.addEventListener('DOMContentLoaded', function() {
                 data: status,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
                     'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
                     'rgba(255, 159, 64, 0.2)'
                 ],
                 borderColor: [
                     'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
                     'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
                     'rgba(255, 159, 64, 1)'
                 ],
                 borderWidth: 1
