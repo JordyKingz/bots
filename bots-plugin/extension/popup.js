832708ec5fea729a8ea475e1f663c3576ca2a666
Jordy de Koning <jordy@nefkon.com>
2016-11-11 03:32:52 +0100

    plugin and website fix

--- bots-plugin/extension/popup.js
+++ bots-plugin/extension/popup.js
@@ -5,16 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
     chrome.tabs.getSelected(null, function(tab) {
       d = document;
 
-      // var f = d.createElement('form');
-      // f.action = 'http://peterschriever.com/api/v1/legal-text-check';
-      // f.method = 'post';
-      // var i = d.createElement('input');
-      // i.type = 'hidden';
-      // i.name = 'url';
-      // i.value = tab.url;
-      // f.appendChild(i);
-      // maak grafiek met f
-      // d.body.appendChild(d.body.innerHTML = 'test');
       $.post( "http://peterschriever.com/api/v1/legal-text-check", function( result ) {
 
         var safe = result.categoriesTotal - result.data.length;
@@ -38,44 +28,21 @@ document.addEventListener('DOMContentLoaded', function() {
             notable+=1;
           }
         });
-        alert(JSON.stringify({safe, notable, hazard}));
+        // alert(JSON.stringify({safe, notable, hazard}));
 
-
-      // will generate array with ['Monday', 'Tuesday', 'Wednesday']
-      alert(JSON.stringify(result));
       var labels = [
         "Veilig",
         "Risicovol",
         "Opvallend",
       ]
 
-      // $.map(result.data, function(category) {
-      //   return category.name;
-      // });
-      // alert(JSON.stringify(labels));
 
       safe = ((safe / 12) * 100).toFixed(1);
       notable = ((notable / 12) * 100).toFixed(1);
       hazard = ((hazard / 12) * 100).toFixed(1);
       var status = [safe, hazard, notable];
 
-      // $.map(result.data, function(category) {
-      //   // weight
-      //   // loop through problems
-      //   let largestProbl = 0;
-      //   $.map(category.paragraphs, function(paragraph) {
-      //     if (paragraph.weight > 66) {
-      //       // largestProbl = paragraph.weight;
-      //       hazard += 1;
-      //       return true;
-      //     }
-      //     if (paragraph.weight > 0) {
-      //       notable += 1;
-      //     }
-      //   });
-      //   return largestProbl;
-      // });
-      alert(JSON.stringify(status));
+
 
       var description = $.map(result.data, function(category) {
         // return category.paragraphs;
@@ -150,9 +117,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 label: '# of Votes',
                 data: status,
                 backgroundColor: [
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(76, 161, 44, 0.8)',
+                    'rgba(191, 17, 13, 0.8)',
+                    'rgba(232, 105, 0, 0.8)'
                 ],
                 borderColor: [
                     'rgba(75, 192, 192, 1)',
